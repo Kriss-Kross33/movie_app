dd79a3462de69798ec296e3bffc80c5040d1f7d9
Kwame Fosu <kwamefosu33@gmail.com>
2021-03-30 07:23:45 +0000

    Add about dialog box

---
+++ movie_app/lib/src/features/about/presentation/widgets/app_dialog.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/src/core/app_color/app_color.dart';
+import 'package:movie_app/src/core/constants/size_constants.dart';
+import 'package:movie_app/src/core/constants/translation_constants.dart';
+import 'package:movie_app/src/core/widgets/button.dart';
+
+import '../../../../core/extensions/size_extension.dart';
+import '../../../../core/extensions/string_extensions.dart';
+
+class AppDialog extends StatelessWidget {
+  final String title, description, buttonText;
+  final Widget child;
+
+  const AppDialog({
+    Key key,
+    @required this.title,
+    @required this.description,
+    @required this.buttonText,
+    this.child,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: AppColor.vulcan,
+      elevation: Sizes.dimen_32.w,
+      insetPadding: EdgeInsets.all(Sizes.dimen_32.w),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(Sizes.dimen_10.w),
+        ),
+      ),
+      child: Container(
+        padding: EdgeInsets.only(
+          top: Sizes.dimen_5.h,
+          left: Sizes.dimen_16.w,
+          right: Sizes.dimen_16.w,
+        ),
+        decoration: BoxDecoration(
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: AppColor.vulcan,
+              blurRadius: Sizes.dimen_16,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              title.translate(context),
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: Sizes.dimen_10.h),
+              child: Text(
+                description.translate(context),
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ),
+            if (child != null)
+              Padding(
+                padding: EdgeInsets.only(bottom: Sizes.dimen_8.h),
+                child: child,
+              ),
+            Button(
+              text: TranslationConstants.OKAY,
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
