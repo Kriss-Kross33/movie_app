0095e1085711dd9f1fd4b8b3fd19ac2a6e4026fc
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-15 12:30:18 +0000

    add movie details to service locator

--- movie_app/lib/src/core/di/service_locator.dart
+++ movie_app/lib/src/core/di/service_locator.dart
@@ -1,16 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 import 'package:movie_app/src/core/api_client/api_client.dart';
-import 'package:movie_app/src/features/home/data/data_sources/movie_remote_data_source.dart';
-import 'package:movie_app/src/features/home/data/repositories/movie_repository_impl.dart';
-import 'package:movie_app/src/features/home/domain/repositories/movie_repository.dart';
-import 'package:movie_app/src/features/home/domain/usecases/get_coming_soon.dart';
-import 'package:movie_app/src/features/home/domain/usecases/get_now_playing.dart';
-import 'package:movie_app/src/features/home/domain/usecases/get_popular.dart';
-import 'package:movie_app/src/features/home/domain/usecases/get_trending.dart';
-import 'package:movie_app/src/features/home/presentation/blocs/movie_backdrop_bloc/movie_backdrop_bloc.dart';
-import 'package:movie_app/src/features/home/presentation/blocs/movie_carousel_bloc/movie_carousel_bloc.dart';
-import 'package:movie_app/src/features/home/presentation/blocs/movie_tabbed_bloc/movie_tabbed_bloc.dart';
+import 'package:movie_app/src/features/features.dart';
 import 'package:movie_app/src/features/languages/presentation/bloc/language_bloc/language_bloc.dart';
 
 final locator = GetIt.I;
@@ -27,18 +18,32 @@ Future setUpLocator() async {
       getPlayingNow: locator(),
     ),
   );
+  locator.registerFactory<MovieDetailBloc>(
+    () => MovieDetailBloc(
+      getMovieDetail: locator(),
+    ),
+  );
   locator.registerSingleton<LanguageBloc>(LanguageBloc());
   //! Usecases
   locator.registerLazySingleton<GetTrending>(() => GetTrending(locator()));
   locator.registerLazySingleton<GetPopular>(() => GetPopular(locator()));
   locator.registerLazySingleton<GetComingSoon>(() => GetComingSoon(locator()));
   locator.registerLazySingleton<GetPlayingNow>(() => GetPlayingNow(locator()));
+  locator
+      .registerLazySingleton<GetMovieDetail>(() => GetMovieDetail(locator()));
   //! Repositories
   locator.registerLazySingleton<MovieRepository>(
       () => MovieRepositoryImpl(remoteDataSource: locator()));
+
+  locator.registerLazySingleton<MovieDetailRepository>(
+      () => MovieDetailRepositoryImpl(remoteDataSource: locator()));
+
   //! Remote Data Sources
   locator.registerLazySingleton<MovieRemoteDataSource>(
       () => MovieRemoteDataSourceImpl(locator()));
+
+  locator.registerLazySingleton<MovieDetailRemoteDataSource>(
+      () => MovieDetailRemoteDataSourceImpl(locator()));
   //! Core
   locator.registerLazySingleton<ApiClient>(() => ApiClient(locator()));
   //! External Packages
