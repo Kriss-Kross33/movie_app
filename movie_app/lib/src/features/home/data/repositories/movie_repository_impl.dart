790719e130bf57e5bf8b2078ae0e05dffb459d94
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-15 12:46:14 +0000

    reorganize imports

--- movie_app/lib/src/features/home/data/repositories/movie_repository_impl.dart
+++ movie_app/lib/src/features/home/data/repositories/movie_repository_impl.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:movie_app/src/core/errors/failures/failures.dart';
-import 'package:movie_app/src/features/home/data/data_sources/movie_remote_data_source.dart';
+import 'package:movie_app/src/features/home/data/datasources/movie_remote_data_source.dart';
 import 'package:movie_app/src/features/home/domain/entities/movie_entity.dart';
 import 'package:movie_app/src/features/home/domain/repositories/movie_repository.dart';
 
