086e47e5d203f1b66f3414bac07193b9fa79d1d2
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-15 12:45:54 +0000

    reorganize imports

--- movie_app/lib/src/features/languages/presentation/bloc/language_bloc/language_bloc.dart
+++ movie_app/lib/src/features/languages/presentation/bloc/language_bloc/language_bloc.dart
@@ -4,7 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/src/core/constants/language_constants.dart';
-import 'package:movie_app/src/features/languages/domain/usecases/language_entity.dart';
+import 'package:movie_app/src/features/languages/domain/entities/language_entity.dart';
 
 part 'language_event.dart';
 part 'language_state.dart';
