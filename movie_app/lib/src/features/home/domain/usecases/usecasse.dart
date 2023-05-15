401946b6c8f9cb185d950e38e8429400a2de5572
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-15 12:42:31 +0000

    remove redundant files

--- movie_app/lib/src/features/home/domain/usecases/usecasse.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:movie_app/src/core/errors/failures/failures.dart';
-
-abstract class Usecase<Type, Params> {
-  Future<Either<Failure, Type>> call(Params params);
-}
