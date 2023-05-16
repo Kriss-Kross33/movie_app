887e6bab03aae550bb10e03c698eb05b8307a805
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-16 07:20:44 +0000

    readd api_constant file

---
+++ movie_app/lib/src/core/constants/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = 'https://api.themoviedb.org/3/';
+  static const String API_KEY = '[YOUR_API_KEY]';
+  static const String BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+  static const String WIRE_DASH_PROJECT_ID = '[YOUR_WIRE_DASH_PROJECT_ID]';
+  static const String WIRE_DASH_SECRET = '[YOUR_WIRE_DASH_SECRET]';
+}
