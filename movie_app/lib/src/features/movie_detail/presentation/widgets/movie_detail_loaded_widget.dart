9f0345a291352ca2f542c52246fc6b39eca623b4
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-17 07:05:25 +0000

    create widget when movie detail is loaded

---
+++ movie_app/lib/src/features/movie_detail/presentation/widgets/movie_detail_loaded_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/src/features/movie_detail/presentation/presentation.dart';
+
+import '../../../../core/core.dart';
+import '../../domain/domain.dart';
+
+class MovieDetailLoadedWidget extends StatelessWidget {
+  const MovieDetailLoadedWidget({
+    super.key,
+    required this.movie,
+    required this.movieDetailArguments,
+  });
+
+  final MovieDetailArguments movieDetailArguments;
+  final MovieDetailEntity movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          MovieDetailImageContainer(
+            movie: movie,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              movie.overview,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.dimen_16.w,
+              vertical: Sizes.dimen_8.h,
+            ),
+            child: Text(
+              TranslationConstants.CAST.translate(context) ??
+                  TranslationConstants.CAST,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          MovieCastWidget(
+            movieDetailArguments: movieDetailArguments,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- movie_app/lib/src/features/movie_detail/presentation/widgets/widgets.dart
+++ movie_app/lib/src/features/movie_detail/presentation/widgets/widgets.dart
@@ -1,2 +1,4 @@
+export 'movie_cast_widget.dart';
 export 'movie_detail_app_bar.dart';
 export 'movie_detail_image_container.dart';
+export 'movie_detail_loaded_widget.dart';
