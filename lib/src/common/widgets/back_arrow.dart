9f8c317c35ba350a8c78f2917eb5e42b213eb4f6
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-18 17:54:38 +0000

    app handler for `onTap`

--- lib/src/common/widgets/back_arrow.dart
+++ lib/src/common/widgets/back_arrow.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:movie_app/src/core/core.dart';
 
 class BackArrow extends StatelessWidget {
-  const BackArrow({super.key});
+  const BackArrow({
+    super.key,
+    this.onTap,
+  });
+
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => Navigator.pop(context),
+      onTap: () => onTap ?? Navigator.pop(context),
       child: Icon(
         Icons.arrow_back_ios,
         color: Colors.white,
