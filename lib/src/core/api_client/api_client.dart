712a3b56e594776bf45fc996653e1c5af2ea3b53
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-18 07:44:44 +0000

    add query option to get method

--- lib/src/core/api_client/api_client.dart
+++ lib/src/core/api_client/api_client.dart
@@ -14,20 +14,33 @@ class ApiClient {
     };
   }
 
-  dynamic get(String path) async {
+  dynamic get(String path, {Map<dynamic, dynamic>? params}) async {
     final headers = getHeaders();
-    final uri = Uri.parse(
-        '${ApiConstants.BASE_URL}$path?api_key=${ApiConstants.API_KEY}');
+    final fullPath = getFullPath(path, params);
+    print('FULLPATH====> $fullPath');
+    final uri = Uri.parse(fullPath);
     final response = await _client.get(
       uri,
       headers: headers,
     );
     if (response.statusCode == 200) {
       final responseBody = jsonDecode(response.body);
-      // print(responseBody);
+      print(responseBody);
       return responseBody;
     } else {
       throw Exception(response.reasonPhrase);
     }
+  }
+
+  String getFullPath(String path, Map<dynamic, dynamic>? params) {
+    String paramString = '';
+    if (params != null) {
+      if (params.isNotEmpty) {
+        params.forEach((key, value) {
+          paramString += '&$key=$value';
+        });
+      }
+    }
+    return '${ApiConstants.BASE_URL}$path?api_key=${ApiConstants.API_KEY}$paramString';
   }
 }
