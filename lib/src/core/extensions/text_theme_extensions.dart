e08d29e385feecdcc06116e6f86c598f7fd3cd67
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-19 13:35:17 +0000

    add new custom textstyles

--- lib/src/core/extensions/text_theme_extensions.dart
+++ lib/src/core/extensions/text_theme_extensions.dart
@@ -17,4 +17,16 @@ extension ThemeTextExtension on TextTheme {
 
   TextStyle? get vulcanBodyText2 =>
       bodyMedium?.copyWith(color: AppColor.vulcan, fontWeight: FontWeight.w600);
+
+  TextStyle? get greySubtitle1 => titleMedium?.copyWith(
+        color: Colors.grey,
+      );
+
+  TextStyle? get violetHeadline6 => titleLarge?.copyWith(
+        color: AppColor.violet,
+      );
+
+  TextStyle? get greyCaption => bodySmall?.copyWith(
+        color: Colors.grey,
+      );
 }
