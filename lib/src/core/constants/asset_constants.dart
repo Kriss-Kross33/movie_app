2a53e179e527cf70815c96f1a2e6b1d311c5dfcf
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-25 19:16:53 +0000

    add background image path to AssetConsts

--- lib/src/core/constants/asset_constants.dart
+++ lib/src/core/constants/asset_constants.dart
@@ -2,4 +2,8 @@ class AssetConstants {
   AssetConstants._();
 
   static const String noImage = 'assets/images/pngs/no_image.png';
+
+  static const String movieBg = 'assets/images/movie_bg.jpeg';
+
+  static const String movieBg2 = 'assets/images/movie_bg_2.jpeg';
 }
