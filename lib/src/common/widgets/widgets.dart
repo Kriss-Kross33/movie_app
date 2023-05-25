f3d423a600bf590e2b374232a9f06b132ec19c1a
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-05-25 19:16:10 +0000

    create widget to add gradient to border

--- lib/src/common/widgets/widgets.dart
+++ lib/src/common/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export 'app_error_widget/app_error_widget.dart';
 export 'back_arrow.dart';
 export 'button.dart';
+export 'container_border_gradient/container_border_gradient.dart';
 export 'movie_app_bar.dart';
 export 'wiredash/wiredash.dart';
